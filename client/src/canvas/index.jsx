45c9b9319ea0b8010a4ede1a5044e3d38cfa5564
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-08-05 23:37:30 +0200

    Push Changes upstream

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -4,9 +4,22 @@ import { Environment, Center } from "@react-three/drei";
 import Shirt from "./Shirt";
 import Backdrop from "./Backdrop";
 import CamerRig from "./CameraRig";
+import CameraRig from "./CameraRig";
 
 const CanvasModel = () => {
-  return <div>Canvas</div>;
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+
+      {/* <CameraRig> */}
+      {/* <Backdrop /> */}
+      <Center>
+        <Shirt />
+      </Center>
+      {/* </CameraRig> */}
+    </Canvas>
+  );
 };
 
 export default CanvasModel;
