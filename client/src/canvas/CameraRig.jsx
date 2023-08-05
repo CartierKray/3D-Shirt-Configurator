666405ce6cf8e980d875efdd96baadbcfd6a793b
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-08-06 00:18:17 +0200

    Adding the Shirt's Camera position 3D

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -10,6 +10,22 @@ const CameraRig = ({ children }) => {
   const snap = useSnapshot(state);
 
   useFrame((state, delta) => {
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+
+    // Set the initial position of the model
+    let targetPosition = [-0.4, 0, 2];
+    if (snap.intro) {
+      if (isBreakpoint) targetPosition = [0, 0, 2];
+      if (isMobile) targetPosition = [0, 0.2, 2.5];
+    } else {
+      if (isMobile) targetPosition = [0, 0, 2.5];
+      else targetPosition = [0, 0, 2];
+    }
+
+    // Set model camera position
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
+
     // Set the model rotation smoothly
     easing.dampE(
       group.current.rotation,
