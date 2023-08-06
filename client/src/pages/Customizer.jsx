f1a260128388300dba9a923e1816a8140d48346c
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-08-06 20:09:44 +0200

    Push Changes Upstream

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -54,7 +54,20 @@ const Customizer = () => {
     if (!prompt) return alert("please enter a prompt");
 
     try {
-      // call our backend to generate an ai image!
+      setGeneratingImg(true);
+      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          prompt,
+        }),
+      });
+
+      const data = await response.json();
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`);
     } catch (error) {
       alert(error);
     } finally {
