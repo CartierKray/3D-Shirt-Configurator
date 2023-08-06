43245ded37bfd040ff4fe5a6d6a025b62ad11ca6
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-08-06 15:28:13 +0200

    Add Server & Routes

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -37,9 +37,29 @@ const Customizer = () => {
       case "filepicker":
         return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
       case "aipicker":
-        return <AIPicker />;
+        return (
+          <AIPicker
+            prompt={prompt}
+            setPrompt={setPrompt}
+            generatingImg={generatingImg}
+            handleSubmit={handleSubmit}
+          />
+        );
       default:
         return null;
+    }
+  };
+
+  const handleSubmit = async (type) => {
+    if (!prompt) return alert("please enter a prompt");
+
+    try {
+      // call our backend to generate an ai image!
+    } catch (error) {
+      alert(error);
+    } finally {
+      setGeneratingImg(false);
+      setActiveEditorTab("");
     }
   };
 
